e8cac553fa6aaf98327a94294a0324d0566f35a3
dougl <dougl@DouglasFM>
2017-11-09 17:25:37 -0300

    Aleluia foi

---
+++ GitHub/src/github/GitHub.java
@@ -0,0 +1,14 @@
+
+public class GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Estado da Lampada: ");
+        //lampada1.piscar(5);
+
+        System.out.println("Estado da Lampada: ");
+        //lampada1.piscar(5);
+    }
+    }
